3aaf90c6b555c9b4c4a0ad644f2c4659774ac7f1
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-08 23:56:17 +0900

    [ADD] auth router

---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+const mysql = require('../mysql');
+
+router.get('/kakao', (req, res) => {});
+
+router.get('/kakao/callback', (req, res) => {});
+
+module.exports = router;
