7a22b9aa6ab10a976ac4f7968bdaefa7bb1d1905
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-17 20:47:24 +0900

    [UPDATE] question.js (get)

--- server/routes/question.js
+++ server/routes/question.js
@@ -4,15 +4,24 @@ const router = express.Router();
 const mysql = require('../mysql');
 
 router.get('/', async (req, res) => {
-  const { type } = req.query;
+  try {
+    const { type } = req.query;
 
-  const questionType = {
-    host: 'hostQuestionList',
-    common: 'commonQuestionList',
-  }[type];
+    const questionType = {
+      host: 'hostQuestionList',
+      common: 'commonQuestionList',
+    }[type];
 
-  const questionList = await mysql.query(questionType);
-  res.send(questionList);
+    const questionList = await mysql.query(questionType);
+    const response = {
+      code: 200,
+      message: 'ok',
+      result: questionList,
+    };
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
