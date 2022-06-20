6bbe6163d58114814bf24301ac92d2e7d4617464
ding-co <pangoons@naver.com>
2022-06-21 07:20:25 +0900

    [ADD] babsangSpoons - API

--- server/routes/babsang.js
+++ server/routes/babsang.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 });
 
 // 밥상 상세정보 가져오기
-router.get('/:id', async (req, res) => {
+router.get('/:id(\\d+)', async (req, res) => {
   try {
     const { id } = req.params;
     const babsangDetail = await mysql.query('babsangDetail', id);
@@ -27,6 +27,94 @@ router.get('/:id', async (req, res) => {
       code: 200,
       message: 'ok',
       result: babsangDetail,
+    };
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+// 밥상_숟갈 목록 가져오기
+router.get('/:id(\\d+)/babsangSpoons', async (req, res) => {
+  try {
+    const babsangSpoonsList = await mysql.query('babsangSpoonsList');
+    const response = {
+      code: 200,
+      message: 'ok',
+      result: babsangSpoonsList,
+    };
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+// 숟갈의 밥상 신청하기
+router.post('/:id(\\d+)/babsangSpoons', async (req, res) => {
+  try {
+    const { type } = req.query;
+    const babsangSpoonsType = {
+      apply: 'babsangSpoonsInsert',
+    }[type];
+    const result = await mysql.query(babsangSpoonsType, req.body.param);
+    const response = {
+      code: 201,
+      message: 'created',
+    };
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+// 숟갈의 밥상 취소하기
+// 밥장의 숟갈 선정하기
+// 밥장의 숟갈 취소하기
+router.put('/:id(\\d+)/babsangSpoons', async (req, res) => {
+  try {
+    const { type } = req.query;
+    const babsangSpoonsType = {
+      applyCancel: 'babsangSpoonsUpdate',
+      pick: 'babsangSpoonsUpdate',
+      pickCancel: 'babsangSpoonsUpdate',
+    }[type];
+    const { id } = req.params;
+    const result = await mysql.query(babsangSpoonsType, [
+      req.body.param,
+      req.body.spoon_email,
+      id,
+    ]);
+    const response = {
+      code: 201,
+      message: 'updated',
+    };
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+// 숟갈 얹은 밥상 가져오기
+// 차려 놓은 밥상 가져오기
+// 선정된 밥상 가져오기
+// 찜한 밥상 가져오기
+router.get('/:id(\\d+)/get', async (req, res) => {
+  try {
+    const { type } = req.query;
+    const babsangGetType = {
+      selectedList: 'babsangSelectedList',
+      appliedList: 'babsangAppliedList',
+      createdList: 'babsangCreatedList',
+      bookmarkedList: 'babsangBookmarkedList',
+    }[type];
+    const babsangGetTypeResult = await mysql.query(
+      babsangGetType,
+      req.body.param
+    );
+    const response = {
+      code: 201,
+      message: 'created',
+      result: babsangGetTypeResult,
     };
     res.send(response);
   } catch (error) {
@@ -71,56 +159,6 @@ router.delete('/:id', (req, res) => {
     const response = {
       code: 204,
       message: 'deleted',
-    };
-    res.send(response);
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-// 밥장의 숟갈 선정하기
-// 숟갈의 밥상 신청하기
-router.post('/', async (req, res) => {
-  try {
-    const { type } = req.query;
-
-    const babSangTreatType = {
-      select: 'babsangSelect',
-      apply: 'babsangApply',
-    }[type];
-
-    const result = await mysql.query(babSangTreatType, req.body.param);
-    const response = {
-      code: 201,
-      message: 'created',
-    };
-    res.send(response);
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-// 숟갈 얹은 밥상 가져오기
-// 차려 놓은 밥상 가져오기
-// 선정된 밥상 가져오기
-// 찜한 밥상 가져오기
-router.get('/', async (req, res) => {
-  try {
-    const { type } = req.query;
-    const babsangGetType = {
-      selectedList: 'babsangSelectedList',
-      appliedList: 'babsangAppliedList',
-      createdList: 'babsangCreatedList',
-      bookmarkedList: 'babsangBookmarkedList',
-    }[type];
-    const babsangGetTypeResult = await mysql.query(
-      babsangGetType,
-      req.body.param
-    );
-    const response = {
-      code: 201,
-      message: 'created',
-      result: babsangGetTypeResult,
     };
     res.send(response);
   } catch (error) {
