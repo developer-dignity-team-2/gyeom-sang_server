fee5befffe68c10d2184a74ca178fd201be8d5f5
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-23 00:04:33 +0900

    [UPDATE] sql.js (update userDetail, profileInsert, scoreInsert)

--- server/mysql/sql.js
+++ server/mysql/sql.js
@@ -1,9 +1,9 @@
 module.exports = {
-  user: `select * from user where email = ?`,
-  userInsert: `insert into user(email, gender, nickname, profile_image, profile_description, age_range)
-                            values(?,?,?,?,?,?,?)`,
+  userDetail: `select * from user where email = ?`,
+  profileInsert: `insert into user set ?`,
   profileDetail: `select * from user u inner join user_question_score_aggregation uqsa on u.email = uqsa.email where u.email = ?`,
   profileUpdate: `update user set ? where email = ?`,
+  scoreInsert: `insert into user_question_score_aggregation set ?`,
   scoreDetail: `select * from user_question_score_aggregation where email = ?`,
   scoreUpdate: `update user_question_score_aggregation set ? where email = ? `,
   hostQuestionList: `select * from host_questions`,
