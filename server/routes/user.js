d32554445ce578cb98f7c44dba0769ebb0cf8eef
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-07-05 01:39:21 +0900

    [REFACTOR] user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -21,27 +21,13 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
-// 사용자 정보 생성하기
-// router.post('/', async (req, res) => {
-//   try {
-//     const result = await mysql.query('userInsert', email);
-//     const response = {
-//       code: 200,
-//       message: 'ok',
-//     };
-
-//   } catch (error) {
-//     res.send(error);
-//   }
-// });
-
 // 사용자 정보 수정하기
 router.put('/', auth, async (req, res) => {
   try {
     const { param } = req.body;
     const { email } = req.decoded;
 
-    const result = await mysql.query('userUpdate', [param, email]);
+    await mysql.query('userUpdate', [param, email]);
     const response = {
       code: 201,
       message: 'updated',
