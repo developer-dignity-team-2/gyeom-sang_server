79269cc46d0f73d80df1b1cd49b386433224500f
ding-co <pangoons@naver.com>
2022-07-06 20:49:03 +0900

    [UPDATE] sql.js

--- server/mysql/sql.js
+++ server/mysql/sql.js
@@ -12,7 +12,7 @@ module.exports = {
   babsangListSearch: `select t1.*, t2.nickname, t2.profile_image from dining_table t1 inner join user t2 on t1.host_email = t2.email where t1.restaurant_name like ?`,
   babsangDetail: `select t1.*, t2.nickname, t2.age_range, t2.profile_image, t2.profile_description, t3.dining_score,
   (select count(*) from dining_table_spoons where dining_table_id = ?) as spoon_count from dining_table t1
-  inner join user t2 on tx1.host_email = t2.email inner join user_question_score_aggregation t3 on t1.host_email = t3.email where id = ?`,
+  inner join user t2 on t1.host_email = t2.email inner join user_question_score_aggregation t3 on t1.host_email = t3.email where id = ?`,
   babsangInsert: `insert into dining_table set ?`,
   babsangUpdate: `update dining_table set ? where id = ?`,
   babsangDelete: `delete from dining_table where id = ? `,
