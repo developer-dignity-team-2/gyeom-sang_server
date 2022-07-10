5643ca62d3ec554fa79aad4fb0d63f94757647f2
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-07-10 18:21:22 +0900

    [REFACTOR] auth.js (delete get)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const axios = require('axios');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
@@ -7,58 +6,6 @@ const router = express.Router();
 const mysql = require('../mysql');
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
-
-// 카카오 access_token으로 jwt 토큰 발급
-router.get('/kakao/signin', async (req, res) => {
-  const token = req.headers.authorization;
-
-  try {
-    const user = await axios({
-      method: 'GET',
-      url: 'https://kapi.kakao.com/v2/user/me',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const {
-      properties: { nickname, profile_image: profileImage },
-      kakao_account: { gender, email, age_range: ageRange },
-    } = user.data;
-
-    const userInfo = {
-      email,
-      gender: gender.slice(0, 1).toUpperCase(),
-      nickname,
-      profile_image: profileImage,
-      profile_description: '편하게 작성해주세요!',
-      age_range: ageRange,
-    };
-
-    const dbUser = await mysql.query('userDetail', email);
-
-    if (dbUser.length < 1) {
-      await mysql.query('userInsert', userInfo);
-      await mysql.query('scoreInsert', { email });
-    }
-
-    const jwtToken = jwt.sign(
-      {
-        type: 'JWT',
-        email,
-      },
-      SECRET_KEY,
-      {
-        expiresIn: '180m',
-        issuer: 'babsang',
-      }
-    );
-
-    res.send(jwtToken);
-  } catch (error) {
-    res.send(error);
-  }
-});
 router.post('/kakao/signup', async (req, res) => {
   // const token = req.headers.authorization;
 
@@ -70,18 +17,6 @@ router.post('/kakao/signup', async (req, res) => {
       email,
       age_range: ageRange,
     } = req.body.param;
-    // const user = await axios({
-    //   method: 'GET',
-    //   url: 'https://kapi.kakao.com/v2/user/me',
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    // });
-
-    // const {
-    //   properties: { nickname, profile_image: profileImage },
-    //   kakao_account: { gender, email, age_range: ageRange },
-    // } = user.data;
 
     const userInfo = {
       email,
