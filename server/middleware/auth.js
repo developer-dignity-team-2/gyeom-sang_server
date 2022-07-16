af5960c6c5302f8eace279b18d4da43009714d62
ding-co <pangoons@naver.com>
2022-07-16 18:12:19 +0900

    [UPDATE] Middleware - auth.js 02

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -14,14 +14,16 @@ const auth = (req, res, next) => {
       });
     }
     const jwtToken = req.headers.authorization.split(' ')[1];
-
     req.decoded = jwt.verify(jwtToken, SECRET_KEY);
-
     return next();
   } catch (error) {
     // 인증 실패
     // 유효시간이 초과된 경우
     if (error.name === 'TokenExpiredError') {
+      if (req.originalUrl === '/api/v1/babsang') {
+        req.decoded = { email: 'nothing' };
+        return next();
+      }
       return res.status(419).json({
         code: 419,
         message: '토큰이 만료되었습니다.',
@@ -29,6 +31,10 @@ const auth = (req, res, next) => {
     }
     // 토큰의 비밀키가 일치하지 않는 경우
     if (error.name === 'JsonWebTokenError') {
+      if (req.originalUrl === '/api/v1/babsang') {
+        req.decoded = { email: 'nothing' };
+        return next();
+      }
       return res.status(401).json({
         code: 401,
         message: '유효하지 않은 토큰입니다.',
